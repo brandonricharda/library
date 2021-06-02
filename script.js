4788fd21c0420c3afa745a779a14169323e699f4
brandonricharda <brandonricharda@gmail.com>
2021-06-01 21:43:11 -0400

    Add functionality for user to create new book and have it display

--- script.js
+++ script.js
@@ -7,19 +7,24 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
+// Methods that facilitate adding book to library
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// Methods that facilitate displaying book in library
+
 function displayBooks() {
 
-    // Creates "bookshelf" that will contain visual display of all books
     let bookshelf = document.getElementById("bookshelf");
+
+    bookshelf.innerHTML = "";
 
     // Loops through each book in library
     myLibrary.forEach(function(book) {
 
-        // Creates a card (div) for the book and adds the right class to it
+        // Creates a div for the book
         let bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
 
@@ -28,20 +33,87 @@ function displayBooks() {
 
         // Loops through the book's properties and adds each one to the list
         for (const property in book) {
-            let propertyListItem = document.createElement("li");
-            propertyListItem.innerHTML = property.toUpperCase() + ": " + book[property];
 
-            // Appends property to the book list
+            // Creates list item and populates its value
+            let propertyListItem = document.createElement("li");
+            propertyListItem.innerHTML = property.toLocaleUpperCase() + ": " + book[property];
+
+            // Appends list item to list
             bookCardList.appendChild(propertyListItem);
 
-            // Appends list to the book card
+            // Appends list to book card
             bookCard.appendChild(bookCardList);
         }
 
         // Appends book card to the bookshelf
         bookshelf.appendChild(bookCard);
+
     });
 }
+
+// Methods that facilitate creating new books
+
+function createBookForm() {
+    
+    // Selects form div and clears it of all inputs and submit buttons before creating new ones
+    let formContainer = document.getElementById("form");
+    clearBookForm();
+    
+    // Creates array of input field ids
+    let ids = ["title", "author", "pages", "read"]
+
+    // Creates and appends inputs with id from ids array
+    for (i = 0; i < ids.length; i++) {
+        let newInput = document.createElement("input");
+        newInput.setAttribute("id", ids[i]);
+
+        if (ids[i] == "read") {
+            newInput.setAttribute("type", "checkbox");
+            let label = document.createElement("p");
+            label.innerHTML = "Read?";
+            label.setAttribute("id", "label");
+            formContainer.appendChild(label);
+        } else {
+            newInput.placeholder = ids[i];
+        }
+
+        formContainer.appendChild(newInput);
+    }
+
+    // Creates submit button for the new book form
+    let submitButton = document.createElement("button");
+    submitButton.innerHTML = "ADD BOOK";
+    submitButton.setAttribute("id", "submit")
+    formContainer.appendChild(submitButton);
+
+    submitButton.addEventListener("click", function() {
+
+        // Collects values from each of the form fields
+        let title = document.getElementById("title").value;
+        let author = document.getElementById("author").value;
+        let pages = document.getElementById("pages").value;
+        let read = document.getElementById("read").value;
+
+        // Creates new book using collected values
+        let newBook = new Book(title, author, pages, read);
+
+        // Calls method that adds new book to library
+        addBookToLibrary(newBook);
+        displayBooks();
+        clearBookForm();
+
+    });
+
+}
+
+function clearBookForm() {
+    let formContainer = document.getElementById("form");
+    formContainer.querySelectorAll("input").forEach(input => input.remove());
+    formContainer.querySelectorAll("#submit").forEach(button => button.remove());
+    formContainer.querySelectorAll("#label").forEach(label => label.remove());
+}
+
+// Page setup methods
 
 function activateNewBookButton() {
     newBookButton = document.getElementById("new-book-button");
@@ -50,24 +122,8 @@ function activateNewBookButton() {
     });
 }
 
-function createBookForm() {
-    let formContainer = document.getElementById("form");
+// Demo setup methods
 
-    // Clears div of all inputs before creating new ones
-    formContainer.querySelectorAll("input").forEach(input => input.remove());
-    
-    // Creates array of input field ids
-    let ids = ["title", "author", "pages", "read"]
-
-    // Creates and appends inputs with id from ids array
-    for (i = 0; i < ids.length; i++) {
-        let newInput = document.createElement("input")
-        newInput.classList.add(ids[i])
-        formContainer.appendChild(newInput);
-    }
-}
-
-// Creates books for demonstration
 bookOne = new Book("20 Things to Know About Money in Your 20s", "Brandon-Richard Austin", 100, false);
 bookTwo = new Book("How to Fail At Everything and Still Win Big", "Scott Adams", 100, true);
 
